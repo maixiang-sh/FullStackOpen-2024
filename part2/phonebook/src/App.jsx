901caa956630dc442932e8bd08d3c389fbacfb61
Mai Xiang <maixiang520@gmail.com>
2024-05-14 15:55:40 +0800

    Finished Exercise 2.11.'

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   // 更新输入框 Query 文本的函数
   const handleQueryChange = (event) => {
@@ -25,7 +27,6 @@ const App = () => {
   // const handleNumberChange = (event) => {
   //   setNewNumber(event.target.value);
   // };
-
   // // 更新输入框 number 文本的函数
   // const handleNameChange = (event) => {
   //   setNewName(event.target.value);
