08ebd5791a6ff73d35236908cfe152402d944185
Mai Xiang <maixiang520@gmail.com>
2024-05-14 18:18:13 +0800

    Finished Part2 d

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,10 +8,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -21,16 +19,39 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      // id: notes.length + 1, id 最好由服务器生成
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      // 服务器响应后，更新本地 notes
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     // console.log(event.target.value);
     setNewNote(event.target.value);
+  };
+
+  // 切换 note 的重要性
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`);
+    const note = notes.find((n) => n.id === id);
+    // 创建 note 的副本， 并将其中的 important 属性取反
+    const changedNote = { ...note, important: !note.important };
+
+    // put 方法用于 替换对应 id 的数据
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        // 必须要通过
+        setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   // showAll 为 true 时，notesToShow 的值为 notes，为 False 时，值为 筛选重要的 notes
@@ -48,7 +69,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
